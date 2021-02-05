2a11c8d64032ab1d0d5248afa64435ce169d1922
Sylwia Calka <scalka@mirabeau.nl>
2021-02-05 11:08:41 +0100

    feat(next) pre-rendering and data fetching

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,22 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layouts/layout';
 import utilStyles from '../styles/utils/utils.module.scss';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+//To use Server-side Rendering, you need to export getServerSideProps(context) instead of getStaticProps from your page.
+export async function getStaticProps() {
+  // Get external data from the file system, API, DB, etc.
+  const allPostsData = getSortedPostsData();
+  // The value of the `props` key will be
+  //  passed to the `Home` component
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -10,6 +24,20 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>This will be a super cool weekly recipe generator</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
---
+++ posts/pre-rendering.md
@@ -0,0 +1,13 @@
+---
+title: 'Healthy salad'
+date: '2020-01-01'
+---
+
+You might have noticed that each markdown file has a metadata section at the top containing title and date. This is called YAML Front Matter, which can be parsed using a library called gray-matter.
+
+Next.js has two forms of pre-rendering: **Static Generation** and **Server-side Rendering**. The difference is in **when** it generates the HTML for a page.
+
+- **Static Generation** is the pre-rendering method that generates the HTML at **build time**. The pre-rendered HTML is then _reused_ on each request.
+- **Server-side Rendering** is the pre-rendering method that generates the HTML on **each request**.
+
+Importantly, Next.js lets you **choose** which pre-rendering form to use for each page. You can create a "hybrid" Next.js app by using Static Generation for most pages and using Server-side Rendering for others.
