6bacce341caa25fcce069aad65b4a4a4ebdd3f3a
Sylwia Calka <scalka@mirabeau.nl>
2021-02-05 09:26:05 +0100

    feat(nextjs) Starter setup

---
+++ pages/recipes/salad.js
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import Head from 'next/head';
+import Layout from '../../components/layout';
+
+export default function SaladRecipe() {
+  return (
+    <Layout>
+      <Head>
+        <title>Salad recipe</title>
+      </Head>
+      <h1>Salad</h1>
+      <Link href="/">
+        <a className="foo" target="_blank" rel="noopener noreferrer">
+          Return home
+        </a>
+      </Link>
+    </Layout>
+  );
+}
