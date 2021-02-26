0de07922407b870c5aeb617a0755022523061423
Sylwia Calka <scalka@mirabeau.nl>
2021-02-26 15:05:59 +0100

    API routes

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+//Try accessing it at http://localhost:3000/api/hello. You should see {"text":"Hello"}.
 export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+  // req  is an instance of http.IncomingMessage, plus some pre-built middlewares
+  //res is an instance of http.ServerResponse, plus some helper functions
+  res.status(200).json({ text: 'Hello' });
+};
